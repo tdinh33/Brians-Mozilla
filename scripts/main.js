dedbc4724426c4978bc2bd58b000ac7acccc1953
Tuan Khai Dinh <dinhtuankhai1@gmail.com>
2024-07-09 20:53:31 +1000

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+
+
+// personalised welcome message 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
+
